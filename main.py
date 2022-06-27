722790f72e2929d1f6c5dd47385a3c2f2870a82f
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-06-27 18:09:32 +0000

    doc: question added

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 print(fibonacci(5))
+
+
+print("2) Factorial of Number")
+
+def factorial(n):
+  if n == 1:
+    return 1
+  ans = n * factorial(n-1)
+  return ans
+
+
+print(factorial(5))
