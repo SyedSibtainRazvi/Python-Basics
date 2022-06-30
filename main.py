c65d57f9c4cc0d91c3f52c8a1124e03c649a2a9e
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-06-30 18:02:17 +0000

    doc: question added

--- main.py
+++ main.py
@@ -45,3 +45,17 @@ for item in sample_list:
     count_dict[item] = 1
 
 print(count_dict)
+
+arr = [9,8,7,6,5,4,3,2,1]
+print("5) Bubble Sort")
+
+def bubble_sort(arr):
+  n = len(arr)
+  for iter_num in range(n):
+    for current in range(n-1-iter_num):
+      if arr[current] > arr[current + 1]:
+        arr[current], arr[current + 1] = arr[current + 1], arr[current]
+
+bubble_sort(arr)
+print(arr)
+  
