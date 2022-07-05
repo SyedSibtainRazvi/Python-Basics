8f86a8b43377d33c076f56485ccb214adaefaf05
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-07-05 17:36:34 +0000

    doc: question added

--- main.py
+++ main.py
@@ -93,3 +93,11 @@ for i in range(1, row + 1, 1):
   for j in range(1, i + 1):
     print(j, end = ' ')
   print(" ")
+
+
+print("Print pattern")
+
+for num in range(10):
+    for i in range(num):
+        print (num, end=" ")
+    print("\n")
