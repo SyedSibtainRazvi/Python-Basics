fa8140bbdb34171d1c5c763de39125d1de05f375
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-07-06 17:37:04 +0000

    doc: question added

--- main.py
+++ main.py
@@ -123,3 +123,11 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 
 print(set1.intersection(set2))
+
+print("Update set if element does not exist")
+
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+
+set1.difference_update(set2)
+print(set1)
