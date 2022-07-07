48ba8e11a99a47723b7581d5ea7b859749461c43
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-07-07 17:39:17 +0000

    doc: question added

--- main.py
+++ main.py
@@ -131,3 +131,10 @@ set2 = {20, 40, 50}
 
 set1.difference_update(set2)
 print(set1)
+
+print("Print list in reverse")
+list1 = [10, 20, 30, 40, 50]
+
+size = len(list1) - 1
+for i in range(size, -1, -1):
+    print(list1[i])
