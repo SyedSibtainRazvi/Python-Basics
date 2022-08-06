97de040535cff2ec46bd354dd6e9b748cbda821e
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-06 11:12:04 +0000

    doc: duplicate question

--- main.py
+++ main.py
@@ -192,3 +192,17 @@ splitArr(arr, n, position)
   
 for i in range(0, n): 
     print(arr[i], end = ' ')
+
+def dedupe_v1(x):
+  y = []
+  for i in x:
+    if i not in y:
+      y.append(i)
+  return y
+
+def dedupe_v2(x):
+    return list(set(x))
+
+a = [1,2,3,4,3,2,1]
+print(dedupe_v1(a))
+print(dedupe_v2(a))
