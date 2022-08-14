8af41443db72c06af2917e24cccceef685304158
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-14 18:04:20 +0000

    doc: question added

--- main.py
+++ main.py
@@ -219,3 +219,14 @@ def swapList(newList):
 newList = [12, 35, 9, 56, 24]
  
 print(swapList(newList))
+
+def swapPositions(list, pos1, pos2):
+     
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+ 
+# Driver function
+List = [23, 65, 19, 90]
+pos1, pos2  = 1, 3
+ 
+print(swapPositions(List, pos1-1, pos2-1))
