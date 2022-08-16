7adbeab0f3c264add38b377c25254ad04d5ea6e6
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-16 17:41:18 +0000

    doc: question added

--- main.py
+++ main.py
@@ -252,3 +252,16 @@ def simple_interest(p,t,r):
     return si
      
 simple_interest(8, 6, 8)
+
+
+
+def compound_interest(principle, rate, time):
+ 
+    # Calculates compound interest
+    Amount = principle * (pow((1 + rate / 100), time))
+    CI = Amount - principle
+    print("Compound interest is", CI)
+ 
+ 
+# Driver Code
+compound_interest(10000, 10.25, 5)
