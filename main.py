f659bf5f18bf06a5be92321a667c04ad7992ca18
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-16 17:39:47 +0000

    doc: question added

--- main.py
+++ main.py
@@ -240,3 +240,15 @@ def isMonotonic(A):
 A = [6, 5, 4, 4]
   
 print(isMonotonic(A))
+
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+     
+    si = (p * t * r)/100
+     
+    print('The Simple Interest is', si)
+    return si
+     
+simple_interest(8, 6, 8)
