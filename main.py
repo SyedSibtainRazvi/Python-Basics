ccb61570b4f9264bc97bf4e101f6abb0864e7dfe
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-21 15:48:54 +0000

    doc: question added

--- main.py
+++ main.py
@@ -328,3 +328,25 @@ for i in range(1, 11):
         print(i, "is a Fibonacci Number")
     else:
         print(i, "is a not Fibonacci Number ")
+
+
+# Python 3 code to find sum
+# of elements in given array
+
+
+def _sum(arr):
+	sum = 0
+	for i in arr:
+		sum = sum + i
+
+	return(sum)
+
+
+arr = []
+arr = [12, 3, 4, 15]
+
+n = len(arr)
+
+ans = _sum(arr)
+
+print('Sum of the array is ', ans)
