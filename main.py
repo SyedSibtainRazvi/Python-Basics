a574364c0f8f9d7be1c26a3d8d134f8d34293f10
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-22 17:34:23 +0000

    doc: question added

--- main.py
+++ main.py
@@ -350,3 +350,21 @@ n = len(arr)
 ans = _sum(arr)
 
 print('Sum of the array is ', ans)
+
+
+
+
+def largest(arr, n):
+
+	max = arr[0]
+	for i in range(1, n):
+		if arr[i] > max:
+			max = arr[i]
+	return max
+
+
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+Ans = largest(arr, n)
+print("Largest in given array ", Ans)
+
