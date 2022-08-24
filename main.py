0c0ae8661420e4daded3dec33fb79e4002f6293c
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-24 17:36:47 +0000

    doc: question added

--- main.py
+++ main.py
@@ -388,3 +388,15 @@ splitArr(arr, n, position)
 for i in range(0, n):
 	print(arr[i], end = ' ')
 
+
+test_list = [ 1, 4, 5, 7, 8 ]
+
+print ("The list is : " + str(test_list))
+
+
+counter = 0
+for i in test_list:
+	
+	counter = counter + 1
+
+print ("Length of list using naive method is : " + str(counter))
