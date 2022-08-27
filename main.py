69dd3d2dceef1fe675204c88c8f8be587aabb1ac
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-08-27 16:39:34 +0000

    doc: question added

--- main.py
+++ main.py
@@ -432,3 +432,24 @@ list1 = [1, 2, 3]
 list2 = [3, 2, 4]
 print(multiplyList(list1))
 print(multiplyList(list2))
+
+
+
+list1 = [10, 20, 4, 45, 99]
+
+mx = max(list1[0], list1[1])
+secondmax = min(list1[0], list1[1])
+n = len(list1)
+for i in range(2,n):
+	if list1[i] > mx:
+		secondmax = mx
+		mx = list1[i]
+	elif list1[i] > secondmax and \
+		mx != list1[i]:
+		secondmax = list1[i]
+	elif mx == secondmax and \
+		secondmax != list1[i]:
+		secondmax = list1[i]
+
+print("Second highest number is : ",\
+	str(secondmax))
