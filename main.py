5f8b521780942edffbd41d697adec2d09465f9ed
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-01 17:10:57 +0000

    doc: question added

--- main.py
+++ main.py
@@ -510,3 +510,12 @@ for num in range(start, end + 1):
 	
 	if num % 2 != 0:
 		print(num, end = " ")
+
+
+list1 = [11, -21, 0, 45, 66, -93]
+
+for num in list1:
+	
+	if num >= 0:
+	    print(num, end = " ")
+
