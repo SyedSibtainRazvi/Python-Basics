9cff5336f654a7340339b05761feaab988c9ed36
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-01 17:09:39 +0000

    doc question added

--- main.py
+++ main.py
@@ -501,3 +501,12 @@ for num in range(start, end + 1):
 	
 	if num % 2 == 0:
 		print(num, end = " ")
+
+
+
+start, end = 4, 19
+
+for num in range(start, end + 1):
+	
+	if num % 2 != 0:
+		print(num, end = " ")
