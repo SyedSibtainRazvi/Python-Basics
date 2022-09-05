380dcbdfc1e8c57042d433562e03cf7f49cfcdf3
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-05 17:50:35 +0000

    doc: question added

--- main.py
+++ main.py
@@ -535,3 +535,13 @@ for num in range(start, end + 1):
 	
 	if num >= 0:
 		print(num, end = " ")
+
+
+
+list1 = [11, 5, 17, 18, 23, 50]
+
+for ele in list1:
+	if ele % 2 == 0:
+		list1.remove(ele)
+
+print("New list after removing all even numbers: ", list1)
