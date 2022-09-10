96a78a4337b24d09a03ddcf1391193e3d1c1798e
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-10 17:37:47 +0000

    doc: question added

--- main.py
+++ main.py
@@ -545,3 +545,13 @@ for ele in list1:
 		list1.remove(ele)
 
 print("New list after removing all even numbers: ", list1)
+
+
+test_list = [5, 6, [], 3, [], [], 9]
+
+print("The original list is : " + str(test_list))
+
+
+res = [ele for ele in test_list if ele != []]
+
+print("List after empty list removal : " + str(res))
