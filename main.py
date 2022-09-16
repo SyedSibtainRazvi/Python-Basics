4d1d370b8aed83240641ebbc346c2fd5bc441873
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-16 17:07:08 +0000

    doc: question added

--- main.py
+++ main.py
@@ -558,3 +558,20 @@ for ele in test_list:
     res.append(sum)
 
 print("List Integer Summation : " + str(res))
+
+
+
+def Repeat(x):
+ _size = len(x)
+ repeated = []
+ for i in range(_size):
+  k = i + 1
+  for j in range(k, _size):
+   if x[i] == x[j] and x[i] not in repeated:
+    repeated.append(x[i])
+ return repeated
+
+list1 = [10, 20, 30, 20, 20, 30, 40,
+  50, -20, 60, 60, -20, -20]
+print (Repeat(list1))
+
