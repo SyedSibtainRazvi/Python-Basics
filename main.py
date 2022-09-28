415256fdbdf59eb22cf3173bf3ec9eb355959fa8
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-28 17:58:33 +0000

    doc: question added

--- main.py
+++ main.py
@@ -613,3 +613,18 @@ print ("List1 before deleting is : " + str(list1))
 list1 *= 0
 
 print ("List1 after clearing using *= 0: " + str(list1))
+
+
+list1 = [1, 2, 3]
+list2 = [5, 6, 7]
+  
+print ("List1 before deleting is : " + str(list1))
+  
+del list1[:]
+print ("List1 after clearing using del : " + str(list1))
+  
+  
+print ("List2 before deleting is : " + str(list2))
+  
+del list2[:]
+print ("List2 after clearing using del : " + str(list2))
