6c22d0f852acc660cd60e9db43051935a4d38e9a
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-09-29 18:13:17 +0000

    doc: question added

--- main.py
+++ main.py
@@ -628,3 +628,26 @@ print ("List2 before deleting is : " + str(list2))
   
 del list2[:]
 print ("List2 after clearing using del : " + str(list2))
+
+
+# Program to add two matrices using nested loop
+
+X = [[1,2,3],
+	[4 ,5,6],
+	[7 ,8,9]]
+
+Y = [[9,8,7],
+	[6,5,4],
+	[3,2,1]]
+
+
+result = [[0,0,0],
+		[0,0,0],
+		[0,0,0]]
+
+for i in range(len(X)):
+	for j in range(len(X[0])):
+		result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+	print(r)
