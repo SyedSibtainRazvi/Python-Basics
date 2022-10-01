b81559b7cd299e992f976521521ad502e8cd0fb4
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-10-01 17:46:13 +0000

    doc: question added

--- main.py
+++ main.py
@@ -675,3 +675,17 @@ for i in range(len(A)):
 
 for r in result:
 	print(r)
+
+def prod(val) :
+	res = 1
+	for ele in val:
+		res *= ele
+	return res
+
+test_list = [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
+
+print("The original list : " + str(test_list))
+
+res = prod([ele for sub in test_list for ele in sub])
+
+print("The total element product in lists is : " + str(res))
