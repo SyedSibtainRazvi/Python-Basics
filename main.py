dd41ebf7f6d71ea23e7a9be5fffcae2b69a3e76e
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-10-06 16:23:59 +0000

    doc: question added

--- main.py
+++ main.py
@@ -727,3 +727,17 @@ if __name__ == "__main__" :
 
 	check(string)
 
+def removeDuplicate(str):
+	s=set(str)
+	s="".join(s)
+	print("Without Order:",s)
+	t=""
+	for i in str:
+		if(i in t):
+			pass
+		else:
+			t=t+i
+		print("With Order:",t)
+	
+str="HellomynameisHello"
+removeDuplicate(str)
