1c25366d98178960cc4dd2f5cacbc056820ff71f
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-10-15 17:16:12 +0000

    doc: question added

--- main.py
+++ main.py
@@ -820,3 +820,15 @@ def returnSum(myDict):
 
 dict = {'a': 100, 'b': 200, 'c': 300}
 print("Sum :", returnSum(dict))
+
+
+def Merge(dict1, dict2):
+	return(dict2.update(dict1))
+
+
+dict1 = {'a': 10, 'b': 8}
+dict2 = {'d': 6, 'c': 4}
+
+print(Merge(dict1, dict2))
+
+print(dict2)
