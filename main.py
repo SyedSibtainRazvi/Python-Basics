ce3e0ff9a09009420efce0668aa9ef93a57513e9
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-10-16 16:51:47 +0000

    doc: question added

--- main.py
+++ main.py
@@ -832,3 +832,13 @@ dict2 = {'d': 6, 'c': 4}
 print(Merge(dict1, dict2))
 
 print(dict2)
+
+import sys
+
+Tuple1 = ("A", 1, "B", 2, "C", 3)
+Tuple2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
+Tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))
+
+print("Size of Tuple1: " + str(sys.getsizeof(Tuple1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(Tuple2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(Tuple3)) + "bytes")
