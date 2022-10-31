acb7cc9e3d3302bed10bd7e5230578064e432c23
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-10-31 17:25:30 +0000

    doc: question added

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
+from itertools import product
+
+test_dict = {'month' : [1, 2, 3],
+			'name' : ['Jan', 'Feb', 'March']}
+
+print("The original dictionary is : " + str(test_dict))
+
+res = dict(zip(test_dict['month'], test_dict['name']))
+
+print("Flattened dictionary : " + str(res))
+
 def UncommonWords(A, B):
 
 	count = {}
