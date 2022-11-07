629479ca935ba93a8f9ca5a2348d37ba93ce389c
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-11-07 17:44:49 +0000

    doc: question added

--- main.py
+++ main.py
@@ -935,3 +935,19 @@ repl_wrd = 'gfg'
 res = ' '.join([repl_wrd if idx in word_list else idx for idx in test_str.split()])
 
 print("String after multiple replace : " + str(res))
+
+test_tup = (5, 20, 3, 7, 6, 8)
+
+print("The original tuple is : " + str(test_tup))
+
+K = 2
+
+res = []
+test_tup = list(sorted(test_tup))
+
+for idx, val in enumerate(test_tup):
+	if idx < K or idx >= len(test_tup) - K:
+		res.append(val)
+res = tuple(res)
+
+print("The extracted values : " + str(res))
