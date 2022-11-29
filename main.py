53242abd6d033c23bd503c66c54b06f1db60b305
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-11-29 18:11:24 +0000

    doc: question added

--- main.py
+++ main.py
@@ -1004,3 +1004,18 @@ for key in sorted(test_dict):
 	res[key] = sorted(test_dict[key])
 
 print("The sorted dictionary : " + str(res))
+
+def reverse_list(arr):
+	left = 0
+	right = len(arr)-1
+	while (left < right):
+		temp = arr[left]
+		arr[left] = arr[right]
+		arr[right] = temp
+		left += 1
+		right -= 1
+
+	return arr
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+print(reverse_list(arr))
