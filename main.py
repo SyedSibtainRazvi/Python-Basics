f3c6795d10e0541da2a2cb9b2f2370d733c72629
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2022-12-02 17:44:56 +0000

    doc: question added

--- main.py
+++ main.py
@@ -1045,3 +1045,24 @@ size = len(arr)
 selectionSort(arr, size)
 print('The array after sorting in Ascending Order by selection sort is:')
 print(arr)
+
+def bubbleSort(arr):
+	n = len(arr)
+	swapped = False
+	for i in range(n-1):
+		for j in range(0, n-i-1):
+
+			if arr[j] > arr[j + 1]:
+				swapped = True
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+		
+		if not swapped:
+			return
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+	print("% d" % arr[i], end=" ")
