96ddbe8b97af689a2480695303e58bd9b71b1505
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2023-01-19 18:22:37 +0000

    doc: question added

--- main.py
+++ main.py
@@ -1073,3 +1073,22 @@ list1 = [1, 2, 3]
 list2 = [3, 2, 4]
 print(multiplyList(list1))
 print(multiplyList(list2))
+
+
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+print()
+
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+print()
+
+a, b = input("Enter two values: ").split()
+print("First number is {} and second number is {}".format(a, b))
+print()
+
+x = list(map(int, input("Enter multiple values: ").split()))
+print("List of students: ", x)
